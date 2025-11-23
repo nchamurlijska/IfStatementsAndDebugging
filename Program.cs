6f9694ce8815fa63218c055ec5a3541b335d5a0a
n-chami <nikolina_chamurlijska@abv.bg>
2025-11-23 12:27:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DebuggingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int score = 85;
+            
+            if (score > 90)
+
+            {
+                Console.WriteLine("Great job!");
+            }
+        }
+    }
+}
